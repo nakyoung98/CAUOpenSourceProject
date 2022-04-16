249130bb2cb2e81627e5319cf431daee8596f7bf
AntoinePoisson <antoine.poisson@epitech.eu>
2022-04-16 20:46:08 +0900

    add: global variable file

--- constant.py
+++ constant.py
@@ -2,4 +2,8 @@
 
 WIDTH, HEIGHT = 920, 920
 
-Orientation = {'up': 0, 'down': 1, 'left': 2, 'right': 3}
+Orientation = {'up': 0, 'down': 180, 'left': 90, 'right': 270}
+
+MODULO_SCREEN = WIDTH / 40
+
+GAMETICK = 6
